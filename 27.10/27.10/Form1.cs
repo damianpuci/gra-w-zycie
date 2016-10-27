d29efb71e9fd9e401fc622cc6e9b61886d3abd36
damianpuci <damianpuci@o2.pl>
2016-10-27 19:33:08 +0200

    with peroid conditions bug

---
+++ 27.10/27.10/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace _27._10
+{
+    public partial class Form1 : Form
+    {
+
+        private static System.Timers.Timer t;
+        int[][] previous;
+        int[][] current;
+        
+        SolidBrush mybrush;
+        Graphics g;
+
+        int n;
+
+
+
+        int is_alive(int current, int a, int b, int c, int d, int e, int f, int g, int h)
+        {
+            int sum = a + b + c + d + e + f + g + h;
+            if (current==0 && sum == 3)
+            {
+                return 1;
+            }
+            if(current == 1 && (sum==2 || sum == 3))
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        void draw(object sender, EventArgs e)
+        {
+            
+                g.Clear(Color.White);
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n ; j++)
+                    {
+
+                    int a = previous[(pictureBox1.Width/5 + i + 1)% (pictureBox1.Width / 5)][(pictureBox1.Height / 5 + j + 1) % (pictureBox1.Width / 5)];
+                    int b = previous[(pictureBox1.Width / 5 + i - 1) % (pictureBox1.Width / 5)][(pictureBox1.Height / 5 + j + 1) % (pictureBox1.Width / 5)] ;
+                    int c = previous[(pictureBox1.Width / 5 + i - 1) % (pictureBox1.Width / 5)][(pictureBox1.Height / 5 + j - 1) % (pictureBox1.Width / 5)];
+                    int d = previous[(pictureBox1.Width / 5 + i + 1) % (pictureBox1.Width / 5)][(pictureBox1.Height / 5 + j - 1) % (pictureBox1.Width / 5)];
+                    int z = previous[i][(pictureBox1.Height / 5 + j - 1) % (pictureBox1.Width / 5)];
+                    int f = previous[i][(pictureBox1.Height / 5 + j + 1) % (pictureBox1.Width / 5)];
+                    int h = previous[(pictureBox1.Width / 5 + i + 1) % (pictureBox1.Width / 5)][j];
+                    int l = previous[((pictureBox1.Width / 5) - i + 1) % (pictureBox1.Width / 5)][j];
+
+                    current[i][j] = is_alive(previous[i][j], a, b, c, d, z, f, h, l);
+
+                        //current[i][j] = is_alive(previous[i][j], previous[i - 1][j - 1], previous[i - 1][j], previous[i][j - 1], previous[i + 1][j], previous[i][j + 1], previous[i + 1][j + 1], previous[i - 1][j + 1], previous[i + 1][j - 1]);
+                }
+                }
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (current[i][j] == 1)
+                        {
+                            g.FillRectangle(mybrush, new Rectangle(i*5, j*5, 5, 5));
+                        }
+                        previous[i][j] = current[i][j];
+                    }
+                }
+            
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Width = 500;
+            pictureBox1.Height = 500;
+
+            mybrush = new SolidBrush(Color.Black);
+            g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+
+
+            n = 100;
+            previous = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                previous[i] = new int[n];
+            }
+
+            current = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                current[i] = new int[n];
+            }
+
+            t = new System.Timers.Timer();
+            t.Interval = 500;
+            t.Elapsed += new System.Timers.ElapsedEventHandler(draw);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            var mouseEventArgs = e as MouseEventArgs;
+            int xCoordinate = mouseEventArgs.X;
+            int yCoordinate = mouseEventArgs.Y;
+
+            Bitmap flag = new Bitmap(500, 500);
+            Graphics flagGraphics = Graphics.FromImage(flag);
+
+            if (previous[yCoordinate / 5][xCoordinate / 5] == 1)
+            {
+                previous[yCoordinate / 5][xCoordinate / 5] = 0;
+            }
+            else
+            {
+                previous[yCoordinate / 5][xCoordinate / 5] = 1;
+            }
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (previous[i][j] == 1)
+                    {
+                        flagGraphics.FillRectangle(Brushes.Black, j * 5, i * 5, 5, 5);
+                    }
+                    else
+                    {
+                        flagGraphics.FillRectangle(Brushes.White, j * 5, i * 5, 5, 5);
+                    }
+                }
+
+                pictureBox1.Image = flag;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+        }
+    }
+}
---
+++ gra_w_zycie/gra_w_zycie/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+
+
+namespace gra_w_zycie
+{
+
+    
+
+
+
+    public partial class Form1 : Form
+    {
+        int method(int a, int b, int c, int n)
+        {
+
+            int r;
+            int[] arr = new int[64];
+            int temp = n;
+
+            for (int i = 0; i < 64; i++)
+            {
+                arr[i] = 0;
+            }
+            int j = 0;
+            while (temp != 0)
+            {
+                r = temp % 2;
+                arr[j++] = r;
+                temp /= 2;
+            }
+
+
+
+            if (a == 1 && b == 1 && c == 1)
+            {
+                return arr[7];
+            }
+            if (a == 1 && b == 1 && c == 0)
+            {
+                return arr[6];
+            }
+            if (a == 1 && b == 0 && c == 1)
+            {
+                return arr[5];
+            }
+            if (a == 1 && b == 0 && c == 0)
+            {
+                return arr[4];
+            }
+            if (a == 0 && b == 1 && c == 1)
+            {
+                return arr[3];
+            }
+            if (a == 0 && b == 1 && c == 0)
+            {
+                return arr[2];
+            }
+            if (a == 0 && b == 0 && c == 1)
+            {
+                return arr[1];
+            }
+            if (a == 0 && b == 0 && c == 0)
+            {
+                return arr[0];
+            }
+
+            return 0;
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            int method_number = (int)numericUpDown1.Value;
+
+            SolidBrush mybrush = new SolidBrush(Color.Black);
+            Graphics g = pictureBox1.CreateGraphics();
+
+            g.Clear(Color.White);
+            const int n = 70;
+
+            int[] current = new int[n];
+            int[] previous = new int[n];
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                current[i] = 0;
+                previous[i] = 0;
+            }
+
+            previous[n / 2] = 1;
+            previous[0] = 1;
+            previous[n - 1] = 1;
+
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if(previous[i]==1)
+                {
+                    g.FillRectangle(mybrush, new Rectangle(i + 1, 0, 1, 1));
+                }
+            }
+
+
+
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (j == 0)
+                    {
+                        current[j] = method(previous[n - 1], previous[j], previous[j + 1], method_number);
+                    }
+                    else if (j == (n - 1))
+                    {
+                        current[j] = method(previous[j - 1], previous[j], previous[0], method_number);
+                    }
+                    else
+                    {
+                        current[j] = method(previous[j - 1], previous[j], previous[j + 1], method_number);
+                    }
+
+                    if (current[j] == 1)
+                    {
+                        g.FillRectangle(mybrush, new Rectangle(j + 1, i + 1, 1, 1));
+                    }
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    previous[j] = current[j];
+                    previous[0] = 1;
+                    previous[n - 1] = 1;
+
+                }
+
+
+            }
+
+
+           
+                
+
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
